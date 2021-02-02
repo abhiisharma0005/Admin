59548f29b7c46720d9c3f4fc5e6428d8a2590071
abhiisharma0005 <65655670+abhiisharma0005@users.noreply.github.com>
2021-02-02 17:26:33 +0530

    Add files via upload

---
+++ mailer.php
@@ -0,0 +1,15 @@
+<?php
+if(isset($_POST['submit'])){
+    $to = "abhiisharma1999@gmail.com"; // this is your Email address
+    $from = $_POST['email']; // this is the sender's Email address
+    $name = $_POST['name'];
+    $subject = "Form submission";
+    $message = $name . " " . " wrote the following:" . "\n\n" . $_POST['comments'];
+
+    $headers = "From:" . $from;
+    $headers2 = "From:" . $to;
+    mail($to,$subject,$message,$headers);
+    echo "Mail Sent. Thank you " . $name . ", we will contact you shortly.";
+    // You can also use header('Location: thank_you.php'); to redirect to another page.
+    }
+?>
