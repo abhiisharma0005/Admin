f94897940450b6fbeba62c33c9126ea64c6dfa65
abhiisharma0005 <65655670+abhiisharma0005@users.noreply.github.com>
2021-08-15 17:10:33 +0530

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -1,19 +1,12 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "abhiisharma1999@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
-?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>INFORMATION</title>
+    <link rel="stylesheet" href="css/style.css">
+  </head>
+  <body>
+    <h2>Thank You For Your Feedback</h2>
+  </body>
+</html>
+    
